64b13627cfe2327076d573d5bc67ee5884ceb122
Joaquin CR <joaquin2010233@gmail.com>
2023-07-13 15:17:43 -0600

    style: adding backgorund color, font family and reddit logo

---
+++ src/app/components/templates/base/BaseTemplate.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react';
+import BaseTemplate, { IBaseTemplate } from './BaseTemplate';
+import { mockBaseTemplateProps } from './BaseTemplate.mocks';
+
+const meta: Meta<typeof BaseTemplate> = {
+  component: BaseTemplate,
+};
+
+export default meta;
+type Story = StoryObj<typeof BaseTemplate>;
+
+export const Primary: Story = {
+  args: {
+    ...mockBaseTemplateProps.base,
+  } as IBaseTemplate,
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'bgColor-Navbar': '#363636',
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient( from 0.5turn, #fb9202, #ff4500 0.5turn,#fb9202)',
+      },
+      fontFamily: {
+        RedexPro: ['Readex'],
       },
     },
   },
   plugins: [],
-}
+};
