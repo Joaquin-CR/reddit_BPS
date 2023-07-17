d6ed7c9c0d26f255e4b61615369aaab3e691700e
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-07-17 15:37:35 -0600

    fix: fix the mobile navbar

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import RedditLogo from '../../../../public/Images/free-reddit-logo-icon.webp';
 
 export default function Navbar() {
   const content = (
-    <div className="bg-bgColor-Navbar text-white w-full h-16 flex items-center">
+    <div className="bg-bgColor-Navbar text-white w-full h-16 flex items-center justify-center lg:justify-start">
       <Image src={RedditLogo} alt="Vercel Logo" className="ml-4" />
       <p className="ml-2 font-RedexPro font-semibold">reddit</p>
     </div>
